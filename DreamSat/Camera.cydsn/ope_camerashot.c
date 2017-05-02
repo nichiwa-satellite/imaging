8e09a54fd36b3977722e0c702a784840091eb1dd
youkan <okabe.tatsuya@nichiwa-system.co.jp>
2017-05-02 17:12:22 +0900

    カメラ撮影関数の追加

--- DreamSat/Camera.cydsn/ope_camerashot.c
+++ DreamSat/Camera.cydsn/ope_camerashot.c
@@ -11,10 +11,23 @@
 */
 
 #include "ope_camerashot.h"
+#include "camera.h"
+
+/* TODO : Recieve Data SIze */
+#define RECIEVE_DATASIZE    ( 10 )
 
 void OpeCameraShot()
 {
-    
+    /* TODO : Set Camera Shot Command */
+    request_digit request[] = "";
+    /* TODO : Recieve Buffer */
+    unsigned char recv_buffer[RECIEVE_DATASIZE];
+
+    /* TODO : ERROR PROCESS */
+    /* REQUEST CAMERASHOT */
+    (void)SendRequest(request, sizeof(request), recv_buffer, sizeof(recv_buffer));
+
+    return;
 }
 
 
