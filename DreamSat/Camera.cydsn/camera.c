aa02886b793f7289472487557714620bc9d8bb82
youkan <okabe.tatsuya@nichiwa-system.co.jp>
2017-05-02 15:15:42 +0900

    カメラへの送信関数/受信関数を追加

--- DreamSat/Camera.cydsn/camera.c
+++ DreamSat/Camera.cydsn/camera.c
@@ -16,29 +16,87 @@
 //Prototypes
 CY_ISR_PROTO(Cam_Rx_Intr);
 
-//struct
-typedef struct recieveinfotag
-{
-    unsigned char   *buffer;
-    unsigned int    buffer_size;
-} recieveinfo;
-
 //Variables
-static unsigned char *recievebuffer;
-static unsigned char recievebuffer_size;
+static unsigned char   *buffer;
+static unsigned int    buffer_size;
+static unsigned int    recieve_size;
 
-rettype Camera_Initialize()
+void Camera_Initialize()
 {
+    /* UART Device Init */
     UART_TO_CAMERA_Init();
     UART_TO_CAMERA_Start();
+
+    /* Interrupt Init */
     Cam_Rx_Intr_StartEx(Cam_Rx_Intr );
+
+    /* Interrupt Disable */
     Cam_Rx_Intr_Disable();
 }
 
-rettype SendRequest(request_digit* request, size_t request_size, unsigned char *buffer, unsigned int buffer_size )
+rettype SendRequest(request_digit* request, size_t request_size, unsigned char *recievebuffer, unsigned int recievebuffer_size )
 {
-    recievebuffer = buffer;
-    recievebuffer_size = buffer_size;
+    /* Parameter Check */
+    if (recievebuffer == NULL) {
+        return ret_error;    
+    }
+
+    /* Parameter Check */
+    if (buffer_size == 0) {
+        return ret_error;
+    }
+
+    /* Recieve Setup */
+    buffer = recievebuffer;
+    buffer_size = recievebuffer_size;
+    recieve_size = 0;
+
+    /* Interrupt Enable */
+    Cam_Rx_Intr_Enable();
+
+    /* Send Request */
+    UART_TO_CAMERA_PutArray(request,request_size);
+
+    /* Recieve Complete Wait */
+    while (1) {
+        /* TODO : TIMEOUT */
+        if (buffer_size <= recieve_size) {
+            /* Interrupt Disanable */
+            Cam_Rx_Intr_Enable();
+            break;
+        }
+    }
+
+    return ret_success;
+}
+
+#pragma interrupt_handler Cam_Rx_Intr
+
+void Cam_Rx_Intr()
+{
+    unsigned int index;
+    unsigned char recv_data;
+
+    /* 1 byte Data Read From UART */
+    recv_data = UART_TO_CAMERA_GetChar();
+
+    /* Set Index */
+    index = recieve_size;
+    
+    /* Parameter Check */
+    if (buffer == NULL) {
+        return;
+    }
+    
+    /* Buffer Overflow */
+    if (buffer_size <= recieve_size) {
+        return;
+    }
+
+    buffer[index] = recv_data;
+    recieve_size++;
+
+    return;
 }
 
 /* [] END OF FILE */
