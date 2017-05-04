709bbe9fc094d726883cd02e5bb26b73b91035bb
youkan <okabe.tatsuya@nichiwa-system.co.jp>
2017-05-04 12:58:04 +0900

    カメラ初期化処理の実装

--- DreamSat/Camera.cydsn/initialize.c
+++ DreamSat/Camera.cydsn/initialize.c
@@ -7,12 +7,14 @@
 */
 #include "project.h"
 #include "communicate_to_earth.h"
+#include "communicate_to_camera.h"
 #include "initialize.h"
 
 void Initialize()
 {
     InitializeUartToEarth();
-    
+    InitializeCamera();
+
     CyGlobalIntEnable;
     
 }
